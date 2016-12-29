9fdde50bfea4584b8b33e74abd0fd495600420d8
Deian Stefan <deian@cs.ucsd.edu>
2016-12-28 23:55:56 -0800

    working on pub

--- rewrite.sh
+++ rewrite.sh
@@ -1,4 +1,5 @@
 #for file in $(git log --name-only --pretty=oneline --full-index HEAD^^..HEAD public | grep -vE '^[0-9a-f]{40} ' | sort | uniq); do
-for file in $(git diff --name-only --pretty=oneline public); do
-  sed -i "s#cse291.programming.systems#cseweb.ucsd.edu/~dstefan/cse291-fall16#" $file  &
+# for file in $(git diff --name-only --pretty=oneline public); do
+for file in $(find public -name *.html); do
+  sed -i "s#cse130.programming.systems#cseweb.ucsd.edu/~dstefan/cse130-winter17#" $file  &
 done
