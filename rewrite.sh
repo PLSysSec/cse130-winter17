447e5d9b286964523e61dd12cd80dce4ab0d329c
Deian Stefan <deian@cs.ucsd.edu>
2017-01-11 00:20:53 -0800

    fix build

--- rewrite.sh
+++ rewrite.sh
@@ -1,5 +1,6 @@
-for file in $(grep -lR cse130.programming.systems public); do
+for file in $(grep -lR cse130.programming.systems public |grep -v .bak | grep -v .pdf); do
+  echo rewriting $file
   sed -i .bak -e "s#cse130.programming.systems#cseweb.ucsd.edu/~dstefan/cse130-winter17#g;" $file
   # sec creates .bak file, let's remove it
-  rm $file.bak
+  rm -f $file.bak
 done
