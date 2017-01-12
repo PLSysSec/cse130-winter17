1424510607139c1576f52a5e9445896a52cebd36
Deian Stefan <deian@cs.ucsd.edu>
2017-01-11 22:44:15 -0800

    lecture 2

--- code/intro-js/class.js
+++ code/intro-js/class.js
@@ -5,10 +5,39 @@ function Car(make, model) {
     return `${this.make}@${this.model}`;
   };
 }
+Car.mySweetProp = 42;
 
 const f = new Car("Ford", "Focus");
 console.log(f.toString());
 const t = new Car("Toyota", "Corola");
 console.log(t.toString());
 
-// TODO: add color to prototype
+// Car.prototype is shared by all objects created by calling new Car(...)
+// That's right you can treat functions like objects!
+
+console.log(f.__proto__ === Car.prototype); // ??
+
+// Let's define property common to all cars:
+Car.prototype.color = "black";
+
+console.log(f.color); // ??
+// getProperty "color" of f
+//     if it has it, return it
+//     else getProperty "color" of f.__proto__
+console.log(t.color); // ??
+
+// Can override the default color that is defined on the prototype:
+
+t.color = "red";
+
+console.log(t.color); // ??
+console.log(f.color); // ??
+
+// We can define a method on the prototype:
+
+Car.prototype.toColorString = function () {
+  return `${this.make}, ${this.model}, ${this.color}`;
+};
+
+console.log(f.toColorString()); // ??
+console.log(t.toColorString()); // ??
--- code/intro-js/receiver.js
+++ code/intro-js/receiver.js
@@ -1,5 +1,6 @@
 const obj = {
-  x: 10,
+  "x-w00t": 10,
+  x: 1337,
   f: function (y) {
     this.x++;
     return this.x + y;
@@ -9,3 +10,4 @@ const obj = {
 console.log(obj.x); // ??
 console.log(obj.f(3)); // ??
 console.log(obj["x"]); // ??
+console.log(obj["x-w00t"]) // ??
